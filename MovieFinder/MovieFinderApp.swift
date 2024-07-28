f7541e296261bbae6bd617bff2f13aa52778af46
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-07-28 17:59:04 +0200

    Initial Commit

---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Mohsin Ali Ayub on 28.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
