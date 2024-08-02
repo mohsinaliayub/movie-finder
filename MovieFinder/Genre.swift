4a0d93d1ef34e249da293a7ed4f78c56bac11066
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-03 00:09:13 +0200

    add models for the movie

---
+++ MovieFinder/Genre.swift
@@ -0,0 +1,16 @@
+//
+//  Genre.swift
+//  MovieFinder
+//
+//  Created by Mohsin Ali Ayub on 02.08.24.
+//
+
+import Foundation
+
+/// **TMDB API** genre for the movie.
+struct Genre {
+    /// Unique identifier for a genre.
+    let id: Int
+    /// Official name of the genre.
+    let name: String
+}
---
+++ MovieFinder/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  MovieFinder
+//
+//  Created by Mohsin Ali Ayub on 02.08.24.
+//
+
+import Foundation
