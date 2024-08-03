1ac7e8e193071b3145b1f93276b6355f80fce181
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-03 23:11:17 +0200

    fetch genres from the api

--- MovieFinder/ContentView.swift
+++ MovieFinder/ContentView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+extension String: Error {
+    
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -16,6 +20,24 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .task {
+            do {
+                try await downloadGenres()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func downloadGenres() async throws {
+        let apiKey = "PUT YOUR API KEY HERE"
+        let urlString = "https://api.themoviedb.org/3/genre/movie/list?api_key=\(apiKey)"
+        guard let url = URL(string: urlString) else { return }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        if (response as? HTTPURLResponse)?.statusCode != 200 { throw "response is not 200" }
+        let genreResponse = try JSONDecoder().decode(GenreResponse.self, from: data)
+        print(genreResponse.genres.prefix(4))
     }
 }
 
