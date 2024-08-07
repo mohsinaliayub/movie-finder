e7112824b20edd22fecc8304362aef56c4273812
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-08 00:33:48 +0200

    change base URL for poster downloads   reduced the size from original -> w500   uses less data and improves app performance

--- MovieFinder/Common/Constants.swift
+++ MovieFinder/Common/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum Constants {
     enum ApiConstants {
-        static let baseURLForImage = "https://image.tmdb.org/t/p/original"
+        static let baseURLForImage = "https://image.tmdb.org/t/p/w500"
         /// The API Key used to make all requests.
         ///
         /// Go to [TMDB](https://themoviedb.org/settings/api) and copy the API Key.
