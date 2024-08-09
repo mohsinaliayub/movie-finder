0a2731ca9a7692a2a0931c58b87411cb1103bbf5
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-09 23:46:48 +0200

    add framework project for API

---
+++ TmdbApi/TmdbApi/TmdbApi.h
@@ -0,0 +1,18 @@
+//
+//  TmdbApi.h
+//  TmdbApi
+//
+//  Created by Mohsin Ali Ayub on 09.08.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TmdbApi.
+FOUNDATION_EXPORT double TmdbApiVersionNumber;
+
+//! Project version string for TmdbApi.
+FOUNDATION_EXPORT const unsigned char TmdbApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TmdbApi/PublicHeader.h>
+
+
