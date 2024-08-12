6aea63a5266d67824e4dab7de04bb8af0a1741a2
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-12 04:29:16 +0200

    update trending movies

--- TmdbApi/TmdbApi/Repositories/Trending/TmdbTrendingMovies.swift
+++ TmdbApi/TmdbApi/Repositories/Trending/TmdbTrendingMovies.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
-public class TmdbTrendingMovies {
+public class TmdbTrendingMovies: TrendingMoviesRepository {
+    var url: URL?
     private let session: URLSession
+    
+    /// A successful response status code for an HTTP request.
+    private let requestSuccessful = 200
     
     public init(session: URLSession = .shared) {
         self.session = session
@@ -23,6 +27,19 @@ public class TmdbTrendingMovies {
         }
         
         // request is successful, deserialize data
+        let moviesResult = try JSONDecoder().decode(MovieOverviewResult.self, from: data)
+        return moviesResult.movies
+    }
+    
+    public func fetchTrending() async throws -> [MovieOverview] {
+        guard let url else { throw TmdbError.invalidURL }
+        
+        let (data, response) = try await session.data(from: url)
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != requestSuccessful {
+            throw TmdbError.error(from: httpResponse)
+        }
+        
+        // request is successful, decode data
         let moviesResult = try JSONDecoder().decode(MovieOverviewResult.self, from: data)
         return moviesResult.movies
     }
