378763c1b6a914515ea9f095a2cbf1cd6939a6a1
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-13 22:23:08 +0200

    add test file for movie details

---
+++ TmdbApi/TmdbApiTests/Movie Details Tests/MovieDetailsTests.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailsTests.swift
+//  TmdbApiTests
+//
+//  Created by Mohsin Ali Ayub on 13.08.24.
+//
+
+import XCTest
+@testable import TmdbApi
+
+final class MovieDetailsTests: XCTestCase {
+    
+}
